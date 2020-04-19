3c554058f4608b19ff3581b5a8853dc71d6b7242
Narendran S Nair <unninarendran36@gmail.com>
2020-04-19 21:32:47 +0530

    Update Repeated String.py

--- Interview Preparation Kit/Warm-up Challenges/Repeated String.py
+++ Interview Preparation Kit/Warm-up Challenges/Repeated String.py
@@ -8,7 +8,7 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
-    return s.count('a')*(n//len(s)) + s[:n%len(s)].count('a')
+    return s.count('a')*(n // len(s)) + s[:n % len(s)].count('a')
 
 
 if __name__ == '__main__':
