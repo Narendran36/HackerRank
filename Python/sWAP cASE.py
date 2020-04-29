3de5c8cc74e25f910dde4d25d72554007518084b
Narendran S Nair <unninarendran36@gmail.com>
2020-04-29 21:44:16 +0530

    Create sWAP cASE.py

---
+++ Python/sWAP cASE.py
@@ -0,0 +1,16 @@
+def swap_case(s):
+    result = ""
+    for i in s:
+        if(i.islower()):
+            result += i.upper()
+        elif(i.isupper()):
+            result += i.lower()
+        else:
+            result += i
+
+    return result
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
