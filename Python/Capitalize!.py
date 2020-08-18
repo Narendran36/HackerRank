f0053b5a4c14595bbc96fe7c76aae69f4feed163
Narendran S Nair <unninarendran36@gmail.com>
2020-08-19 00:48:05 +0530

    Create Capitalize!.py

---
+++ Python/Capitalize!.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    s = s.split(" ")
+    l = [i.capitalize() for i in s]
+    return " ".join(l)
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
